4b2c76ebb8e011bb966ae73c973285fd11504358
Robin Nandi <robin.nandi@gmail.com>
2012-03-14 16:30:40 +0000

    photon plots

--- config.py
+++ config.py
@@ -3,8 +3,59 @@ from utils import PSet
 ECAL_Isolation = PSet(
     ngraphs = 2,
     labels = ["SUSY", "QCD"],
-    datafile = "data/ECAL_Isolation.txt",
+    datafile = "data/ecalIso.txt",
     name = "plots/ECAL_Isolation",
-    xlabel = r"\textbf{ECAL isolation (GeV)}",
-    ylabel = r"\textbf{fraction of events}",
+    xlabel = r"ECAL isolation (GeV)",
+    ylabel = r"fraction of events",
+    cutvalue = 4.2
+)
+
+HCAL_Isolation = PSet(
+    ngraphs = 2,
+    labels = ["SUSY", "QCD"],
+    datafile = "data/hcalIso.txt",
+    name = "plots/HCAL_Isolation",
+    xlabel = r"HCAL isolation (GeV)",
+    ylabel = r"fraction of events",
+    cutvalue = 2.2
+)
+
+Track_Isolation = PSet(
+    ngraphs = 2,
+    labels = ["SUSY", "QCD"],
+    datafile = "data/trackIso.txt",
+    name = "plots/Track_Isolation",
+    xlabel = r"Track isolation (GeV)",
+    ylabel = r"fraction of events",
+    cutvalue = 2.0
+)
+
+Hadronic_Over_EM = PSet(
+    ngraphs = 2,
+    labels = ["SUSY", "QCD"],
+    datafile = "data/hadronicOverEm.txt",
+    name = "plots/Hadronic_Over_EM",
+    xlabel = r"H/E",
+    ylabel = r"fraction of events",
+    cutvalue = 0.05
+)
+
+SigmaIetaIeta_EB = PSet(
+    ngraphs = 2,
+    labels = ["SUSY", "QCD"],
+    datafile = "data/sigmaIetaIeta_EB.txt",
+    name = "plots/SigmaIetaIeta_EB",
+    xlabel = r"Shower Shape",
+    ylabel = r"fraction of events",
+    cutvalue = 0.012
+)
+
+SigmaIetaIeta_EE = PSet(
+    ngraphs = 2,
+    labels = ["SUSY", "QCD"],
+    datafile = "data/sigmaIetaIeta_EE.txt",
+    name = "plots/SigmaIetaIeta_EE",
+    xlabel = r"Shower Shape",
+    ylabel = r"fraction of events",
+    cutvalue = 0.03
 )
--- make_plot.py
+++ make_plot.py
@@ -1,15 +1,24 @@
 print "And we're off ..."
+# Import stuff
 import numpy as np
 import matplotlib.pyplot as plt
-from config import ECAL_Isolation as config
+from config import SigmaIetaIeta_EB as config
 print "Imported files."
 
+# Create the figure
 fig = plt.figure()
 print "Made figure."
+
+# Open the file containing the data
 f = open(config.datafile, "r")
 print "Opened file."
+
+# Declare xvalues and yvalues to be filled
 xvalues = []
 yvalues = [ [] for i in range(config.ngraphs) ] 
+
+# Read data from the file first element is x value bin centre the others are
+# various different graphs.
 print "Loop through lines in file ..."
 j = 1
 for line in f:
@@ -21,22 +30,45 @@ for line in f:
     for i in range(config.ngraphs):
         yvalues[i].append(float(vec[i+1]))
     j = j + 1
+
+# Define the binning
 nbins = len(xvalues)
-binwidth = xvalues[1] - xvalues[0]
-xmin = xvalues[0]-binwidth/2
-xmax = xmin + binwidth*nbins
+binwidth = round(xvalues[1] - xvalues[0], 5)
+xmin = round(xvalues[0]-binwidth/2, 5)
+xmax = round(xmin + binwidth*nbins, 5)
+print xmax
 bins = np.arange(xmin, xmax+binwidth, binwidth)
 print bins
+
+# Plot the graphs
 print "Plotting graphs ..."
 for i in range(config.ngraphs):
     print xvalues
     print yvalues[i]
     plt.hist(xvalues, bins=bins, range=None, normed=False, weights=yvalues[i], histtype="step", label=config.labels[i])
     "Plotted graph "+str(i+1)+"."
+ax = plt.gca()
+print ax.get_ylim()
+ymin = ax.get_ylim()[0]
+ymax = ax.get_ylim()[1]
+
+# Plot a vertical line representing the cut value
+plt.plot([config.cutvalue, config.cutvalue], [ymin, ymax], "k-", label="cut value")
 print "Finished plotting graphs."
+
+# Put in xlabel and ylabel
 plt.xlabel(config.xlabel)
 plt.ylabel(config.ylabel)
+plt.axis([xmin, xmax, ymin, ymax])
 print "Set graph labels."
+
+# Make legend
 plt.legend()
 print "Done legend."
+
+# Write to files
+print "Saving files ..."
 plt.savefig(config.name+".png")
+plt.savefig(config.name+".eps")
+plt.savefig(config.name+".pdf")
+print "Done. Exiting."
