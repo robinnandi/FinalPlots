aa55e25d22a1e0d9c5314ecfe454a6147f5bfb4a
Robin Nandi <robin.nandi@gmail.com>
2012-03-14 01:03:36 +0000

    first working plot

--- config.py
+++ config.py
@@ -1,18 +1,10 @@
 from utils import PSet
 
-config = PSet(
+ECAL_Isolation = PSet(
     ngraphs = 2,
-    types = ["hist", "hist"], # hist or graph
     labels = ["SUSY", "QCD"],
-    colors = ["r", "b"],
-    markers = ["o", "s"],
-    linestyles = ["-", "-"],
-    datafile = "data.txt",
-    name = "ecalIso",
-    xlabel = "ECAL isolation (GeV)",
-    ylabel = "fraction of events",
-    xmin = 0.,
-    xmax = 10.,
-    ymin = 0.,
-    ymax = 1.
+    datafile = "data/ECAL_Isolation.txt",
+    name = "plots/ECAL_Isolation",
+    xlabel = r"\textbf{ECAL isolation (GeV)}",
+    ylabel = r"\textbf{fraction of events}",
 )
--- make_plot.py
+++ make_plot.py
@@ -1,22 +1,42 @@
+print "And we're off ..."
+import numpy as np
 import matplotlib.pyplot as plt
-from config import config
+from config import ECAL_Isolation as config
+print "Imported files."
 
 fig = plt.figure()
+print "Made figure."
 f = open(config.datafile, "r")
+print "Opened file."
 xvalues = []
 yvalues = [ [] for i in range(config.ngraphs) ] 
+print "Loop through lines in file ..."
+j = 1
 for line in f:
-    line.rstrip("\n")
+    print "At line "+str(j)+"."
+    line = line.rstrip("\n")
+    print line
     vec = line.split(", ")
-    xvalues.append(vec[0])
+    xvalues.append(float(vec[0]))
     for i in range(config.ngraphs):
-        yvalues[i].append(vec[i+1])
+        yvalues[i].append(float(vec[i+1]))
+    j = j + 1
 nbins = len(xvalues)
+binwidth = xvalues[1] - xvalues[0]
+xmin = xvalues[0]-binwidth/2
+xmax = xmin + binwidth*nbins
+bins = np.arange(xmin, xmax+binwidth, binwidth)
+print bins
+print "Plotting graphs ..."
 for i in range(config.ngraphs):
-    plt.plot(xvalues, yvalues[i], marker=config.markers[i], linestyle=config.linestyles[i], color=config.colors[i], label=config.labels[i])
+    print xvalues
+    print yvalues[i]
+    plt.hist(xvalues, bins=bins, range=None, normed=False, weights=yvalues[i], histtype="step", label=config.labels[i])
+    "Plotted graph "+str(i+1)+"."
+print "Finished plotting graphs."
 plt.xlabel(config.xlabel)
 plt.ylabel(config.ylabel)
-plt.axis([config.xmin, config.xmax, config.ymin, config.ymax])
+print "Set graph labels."
 plt.legend()
-plt.show()
+print "Done legend."
 plt.savefig(config.name+".png")
